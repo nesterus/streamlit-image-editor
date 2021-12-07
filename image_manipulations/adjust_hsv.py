5d643426395f84d49e87854ab99f1cb1d5e7aa69
NIKITA <nik2003200314@mail.ru>
2021-12-08 01:45:31 +0300

    решение hsv rjb-с abs и Увелеичение

--- image_manipulations/adjust_hsv.py
+++ image_manipulations/adjust_hsv.py
@@ -2,13 +2,19 @@ from utils import *
 
 
 def adjust_hsv(state, block):
-    DEFAULT_HUE = .5
+    default = .5
 
     hsv_img = rgb2hsv(state['image'])
 
-    h = block.slider('Hue', min_value=0., max_value=1., value=DEFAULT_HUE)
-    hsv_img[:, :, 0] += (h - DEFAULT_HUE) % 1.
+    h = block.slider('Hue', min_value=0., max_value=1., value=default)
+    hsv_img[:, :, 0] += (h - default) % 1.
 
     # TODO: adjust saturation and brightness
+
+    s = block.slider('Saturation', min_value=0., max_value=1., value=default)
+    hsv_img[:, :, 1] += (s - default) % 1.
+
+    v = block.slider('Value', min_value=0., max_value=1., value=default)
+    hsv_img[:, :, 2] += (v - default) % 1.
 
     state['image'] = hsv2rgb(hsv_img)
